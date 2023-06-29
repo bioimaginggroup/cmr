dc56b744cfcdf635da81db873d004c56332a2dd6
Volker Schmid <volker.schmid@lmu.de>
2023-06-29 15:53:41 +0200

    Version 1.1: Bullseye plot fixed to AHA segment numbering, allow for 17 segments. Updates for R-4.3

--- R/bullseye.R
+++ R/bullseye.R
@@ -1,6 +1,6 @@
 #' Bullseye plot
 #'
-#' @param x vector of length 16
+#' @param x vector of length 16 or 17
 #' @param lim limits of x values
 #' @param reverse boolean, reverse colors?
 #' @param legend boolean, add legend?
@@ -10,7 +10,8 @@
 #' @return plot
 #' @export
 #' @import fields 
-#' @importFrom graphics polygon par plot
+#' @importFrom graphics polygon par plot 
+#' @importFrom plotrix draw.circle
 #' @examples
 #'   bullseye(1:16)
 bullseye<-function(x, lim=NULL,reverse=TRUE,legend=TRUE,text=TRUE,cex=1)
@@ -18,7 +19,7 @@ bullseye<-function(x, lim=NULL,reverse=TRUE,legend=TRUE,text=TRUE,cex=1)
 oldpar <- par(no.readonly = TRUE)
 on.exit(par(oldpar))
 
-if (length(x)!=16)stop("x has to be of length 16")
+if (length(x)<16|length(x)>17)stop("x has to be of length 16 or 17")
 werte=x
 zlim=lim
 
@@ -35,17 +36,21 @@ par(mai=c(0,0,0,0))
 plot(c(0,1),c(0,1),col="white",axes=FALSE,asp=1,ylab="",xlab="")
  
 for (i in 1:6)
-  graphics::polygon(0.4+0.4*c(cos(-pi/2+pi/6+seq(2*i,2*i+2,by=0.1)*pi/6),0.75*cos(-pi/2+pi/6+seq(2*i+2,2*i,by=-0.1)*pi/6)),
-        0.5+0.4*c(sin(-pi/2+pi/6+seq(2*i,2*i+2,by=0.1)*pi/6),0.75*sin(-pi/2+pi/6+seq(2*i+2,2*i,by=-0.1)*pi/6)),
+  graphics::polygon(0.4+0.4*c(cos(-pi/2+pi/6+seq(2*i+2,2*i+4,by=0.1)*pi/6),
+                              0.75*cos(-pi/2+pi/6+seq(2*i+4,2*i+2,by=-0.1)*pi/6)),
+        0.5+0.4*c(sin(-pi/2+pi/6+seq(2*i+2,2*i+4,by=0.1)*pi/6),
+                  0.75*sin(-pi/2+pi/6+seq(2*i+4,2*i+2,by=-0.1)*pi/6)),
 col=colo[263*ziel[i]+1],border="black")
 for (i in 1:6)
-  graphics::polygon(0.4+0.3*c(cos(-pi/2+pi/6+seq(2*i,2*i+2,by=0.1)*pi/6),0.667*cos(-pi/2+pi/6+seq(2*i+2,2*i,by=-0.1)*pi/6)),
-        0.5+0.3*c(sin(-pi/2+pi/6+seq(2*i,2*i+2,by=0.1)*pi/6),0.667*sin(-pi/2+pi/6+seq(2*i+2,2*i,by=-0.1)*pi/6)),
+  graphics::polygon(0.4+0.3*c(cos(-pi/2+pi/6+seq(2*i+2,2*i+4,by=0.1)*pi/6),0.667*cos(-pi/2+pi/6+seq(2*i+4,2*i+2,by=-0.1)*pi/6)),
+        0.5+0.3*c(sin(-pi/2+pi/6+seq(2*i+2,2*i+4,by=0.1)*pi/6),0.667*sin(-pi/2+pi/6+seq(2*i+4,2*i+2,by=-0.1)*pi/6)),
 col=colo[263*ziel[i+6]+1],border="black")
 for (i in 1:4)
 graphics::polygon(0.4+0.2*c(cos(seq(2*i-1,2*i+1,by=0.1)*pi/4),0.5*cos(seq(2*i+1,2*i-1,by=-0.1)*pi/4)),
         0.5+0.2*c(sin(seq(2*i-1,2*i+1,by=0.1)*pi/4),0.5*sin(seq(2*i+1,2*i-1,by=-0.1)*pi/4)),
 col=colo[263*ziel[i+12]+1],border="black")
+if (length(x)==17)
+  plotrix::draw.circle(0.4,0.5,0.1,col=colo[263*ziel[17]+1],border="black")
 
 
 #for (i in 0:262)
@@ -55,7 +60,6 @@ col=colo[263*ziel[i+12]+1],border="black")
 #bla<-seq(zlim[2],zlim[1],length=5)
 #for (i in 0:4)
 #text(0.93,0.8-i*0.15,round(bla[i+1],1),cex=1,pos=4)
-#dev.off()
 
 if(text){
   par(cex=cex)
